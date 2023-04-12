e38cf6f64677cac46c313682b80b518790d38aa1
Vasilije Roganovic <vaskevs99@gmail.com>
2023-04-12 12:52:04 +0200

    New folder

---
+++ src/app_function/main_function.h
@@ -0,0 +1,6 @@
+void menu_page(int *n);
+void loggin_account(int n, struct person *Person, int num_users);
+void registration_account(int n, struct person *Person, int num_users);
+void file_function(struct person *Person, int num_users, FILE *dat);
+void person_information(struct person *Person, int f_indeks);
+void transfer_function(struct person *Person, int n, int f_current_indeks, int num_users);
